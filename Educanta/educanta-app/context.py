bc8502b1ea2216b94cbd3ada3eb83dea37a600e8
ib-mrm <100607186+ib-mrm@users.noreply.github.com>
2023-01-26 12:23:40 +0100

    Delete Educanta directory

--- Educanta/educanta-app/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
-
-from configs import config  # NOQA
-from utils import databases
-from utils import aws
-from utils import general
--- Educanta/educanta-app/settings_prod.py
+++
@@ -1,2 +0,0 @@
-ENV = 'prod'
-DEBUG = False
